47d858885ba8327e9313669a968ffd95a8d45fa3
Jayamaha <Jayamaha@DESKTOP-P04PH7O>
2021-05-14 16:50:39 +0530

    ImportPlaylists Commit

--- Clementine/src/clementine/internetLogin/InternetLoginController.java
+++ Clementine/src/clementine/internetLogin/InternetLoginController.java
@@ -7,7 +7,15 @@ package clementine.internetLogin;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -15,9 +23,45 @@ import javafx.fxml.Initializable;
  * @author Jayamaha
  */
 public class InternetLoginController implements Initializable {
+    
+    @FXML
+    private TextField password;
+
+    @FXML
+    private TextField uname;
+
+    @FXML
+    private Button login;
+
 
     public void goToSpotifyplaylists(){
-        System.out.println("clicked");
+        String username;
+        String password;
+        username = this.uname.getText().toString();
+        password = this.password.getText().toString();
+        
+        if(username.contains("nadunjayamaha") && password.contains("12345")){
+            try{
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/clementine/importPlaylists/importPlaylists.fxml"));
+            Parent rootX = (Parent) fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setTitle("Virtualization");
+            stage.setScene(new Scene(rootX));
+            stage.show();
+        }
+        catch (Exception e){
+            System.out.println(e);
+        }
+            
+        }
+        else{
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setTitle("Message Here...");
+                alert.setHeaderText("Look, an Information Dialog");
+                alert.setContentText("I have a great message for you!");
+                alert.showAndWait();
+        }
+        
     }
     @Override
     public void initialize(URL url, ResourceBundle rb) {
