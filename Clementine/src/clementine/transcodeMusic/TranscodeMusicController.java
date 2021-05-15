095e09f6abf43ddbef726648f57bc677997cd30f
SahanChan <xnscb8.chandrabahu@gmail.com>
2021-05-15 21:42:13 +0530

    "transcodeMusic_Complete"

--- Clementine/src/clementine/transcodeMusic/TranscodeMusicController.java
+++ Clementine/src/clementine/transcodeMusic/TranscodeMusicController.java
@@ -7,7 +7,12 @@ package clementine.transcodeMusic;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -19,9 +24,32 @@ public class TranscodeMusicController implements Initializable {
     /**
      * Initializes the controller class.
      */
+    
+    @FXML
+    private Button closeButton;
+
+       @FXML
+    private Button addFileButton;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+    }
+
+    @FXML
+    void closeTranscode(ActionEvent event) {
+        Stage stage = (Stage) closeButton.getScene().getWindow();
+        stage.close();
     }    
+    
+     @FXML
+    void openAddFIleBrowser(ActionEvent event) {
+         Stage stage = (Stage) addFileButton.getScene().getWindow();
+
+         FileChooser fileChooser = new FileChooser();
+         fileChooser.setTitle("Add Music Track");
+         fileChooser.showOpenDialog(stage);
+    }
+    
     
 }
